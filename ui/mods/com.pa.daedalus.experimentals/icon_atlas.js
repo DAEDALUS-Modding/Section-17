8a6b75aae92ec461df79f42a459eff87a95f18ab
Thalassocracy <42811940+Thalassocracy@users.noreply.github.com>
2021-02-19 23:55:54 -0500

    Add Spoder

--- ui/mods/com.pa.daedalus.experimentals/icon_atlas.js
+++ ui/mods/com.pa.daedalus.experimentals/icon_atlas.js
@@ -2,5 +2,6 @@ var paeiouIcons = ["experimental_vehicle_gantry",
 "big_bill",
 "pineapple",
 "floater",
+"spoder",
 ];
 model.strategicIcons(model.strategicIcons().concat(paeiouIcons));
--- ui/mods/com.pa.daedalus.experimentals/shared_build.js
+++ ui/mods/com.pa.daedalus.experimentals/shared_build.js
@@ -3,6 +3,7 @@ var newBuild = {
 "/pa/units/PAEIOU/big_bill/big_bill.json": ["vehicle", 0,{ row: 0, column: 4, titans: true }],
 "/pa/units/PAEIOU/pineapple/pineapple.json": ["bot", 0,{ row: 0, column: 5, titans: true }],
 "/pa/units/PAEIOU/floater/floater.json": ["vehicle", 0,{ row: 0, column: 1, titans: true }],
+"/pa/units/PAEIOU/spoder/spoder.json": ["bot", 0,{ row: 1, column: 7, titans: true }],
 
 }
 if (Build && Build.HotkeyModel && Build.HotkeyModel.SpecIdToGridMap) {
